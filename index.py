02e6f8c34792db7ad74d1eecd2fddc64809d1f7d
sanjog <sanjogsinghbhatia2003@gmail.com>
2024-07-12 18:41:17 +0530

    test 2: added some files

--- index.py
+++ index.py
@@ -1,14 +1,1 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-def client_app():
-    # return "Hiiiii"
-    return render_template('app.html')
-
-
-if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction...")
-    app.run()
+from wsgi import app
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from app import app
+
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    app.run()
